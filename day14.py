0d103ac04d43aebffdb2719e6cafed187af5d149
aniket hedau <aniketaniket6287@gmail.com>
2026-01-07 20:31:25 +0530

    new code update

--- day14.py
+++ day14.py
@@ -1,8 +1,69 @@
-#print the table of 2 
-# for i in range (1 ,11):
-#     print("19 x",i,"=",19*i )
+#if elif else 
+# Greatest between three numbers 
+# num1 =30
+# num2 = 50 
+# num3 = 90
+# if num1>num2 and num1>num3 :
+#     print(num1)
+# elif num2>num1 and num2>num3 :
+#     print(num2)
+# else :
+#     print(num3)
 
-for num in range(1, 21):
-    print(f"\nTable of {num}")
-    for i in range(1, 11):
-        print(f"{num} x {i} = {num*i}")
+# # 100 - 80 A garde 
+# # 80 - 60 B garde 
+# # 60 - 30 C garde 
+# # 30 - 0 D garde 
+
+# marks = int(input("enter your marks"))
+# if marks>=80 and marks<100 :
+#     print("A grade")
+# elif marks>=60 and marks<80 :
+#     print("B garde")
+# elif marks>=30 and marks<60 :
+#     print("C garde")
+# elif marks>=0 and marks<30 :
+#     print("D garde")
+# else:
+#     print("invalid input")
+
+# #input income and calculate tax 
+# income = int(input("enter your income"))
+# if income<=0  and income<250000:
+#     print("no tax")
+# elif income>=250001 and income<=500000 :
+#     print("your tax is ",income*5/100)
+# elif income>=500001 and income<=1000000 :
+#     print("your tax is ",income*20/100)
+# elif income>1000000 :
+#     print("your tax is ",income*30/100)
+# else :
+#     print("invalid input")
+
+#input time in 24-hour format and display
+# time = int(input("enter a time"))
+# if time>=0 and time<12 :
+#     print("Good Morning")
+# elif time>=12 and time<17 :
+#     print("Good Afternoon")
+# elif time>=17 and time<20 :
+#     print("Good Evening")
+# else:
+#     print("Good night")
+
+num1 = int(input("enter"))
+num2 = int(input("enter"))
+operation= input("enter")
+if operation=="+":
+    print(num1+num2)
+elif operation=="-":
+    print(num1-num2)
+elif  operation=="*":
+    print(num1*num2)
+elif operation=="/":
+    print(num1/num2)
+
+    
+    
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,11 @@
-range (1 ,11):
-#     print("19 x",i,
+come = int(input("enter your income"))
+# if income<=0  and income<250000:
+#     print("no tax")
+# elif income>=250001 and income<=500000 :
+#     print("your tax is ",income*5/100)
+# elif income>=500001 and income<=1000000 :
+#     print("your tax is ",income*20/100)
+# elif income>1000000 :
+#     print("your tax is ",income*30/100)
+# else :
+#     print("invalid input")
