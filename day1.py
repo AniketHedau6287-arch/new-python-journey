13a547005559d19b459bf4509794d9c875d4c873
aniket hedau <aniketaniket6287@gmail.com>
2026-01-07 14:52:21 +0530

    update in code

--- day1.py
+++ day1.py
@@ -1,4 +1,9 @@
-print("Hello everyone")
-num=10
-num=23
-print(num)
+# print("Hello everyone")
+# num=10
+# num=23
+# print(num)
+
+#add two numbers wit user input 
+num1=int(input("enter any number"))
+num2=int(input("enter any number"))
+print(num1+num2)
