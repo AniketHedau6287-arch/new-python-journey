f142f8a40d44fbc825a5cb4b2213fc54550742c0
aniket hedau <aniketaniket6287@gmail.com>
2026-01-07 15:15:46 +0530

    new update

---
+++ day14.py
@@ -0,0 +1,8 @@
+#print the table of 2 
+# for i in range (1 ,11):
+#     print("19 x",i,"=",19*i )
+
+for num in range(1, 21):
+    print(f"\nTable of {num}")
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num*i}")
