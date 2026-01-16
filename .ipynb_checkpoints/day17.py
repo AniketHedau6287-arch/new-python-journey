91f0cbc3a7e5f90ac29e5299037d38a26c206bbd
aniket hedau <aniketaniket6287@gmail.com>
2026-01-16 20:33:32 +0530

    loop learning

---
+++ .ipynb_checkpoints/day17.py
@@ -0,0 +1,100 @@
+# Loop ( repetated task )
+# for ( when we know the iteration starting and ending (range) )
+# syntax
+# for variable_name in range( start, end-1 , steps ):
+#                  code
+
+# print value from 1 to 10
+
+# for val in range(1,11,1):
+#     print(val,end=" ")
+
+
+# while ( condition base )
+# syntax
+# variable_name = starting point
+# while condition:
+#      code
+#      increment/decrement
+
+# print value from 1 to 10
+
+# val = 1
+# while val <= 10:
+#     print(val,end=" ")
+#     val += 1 
+
+# print(val)
+
+# print 10 to 1 
+# val = 10
+# while val >=1:
+#     print(val,end=" ")
+#     val-=1
+
+#print value from 1 to 10 ?
+# for val in range(1,11,1):
+#     print (val,end=" ")
+
+# val = 1
+# while val <=10:
+#     print(val,end=" ")
+#     val+=1
+    
+#print value from 10 to 1 ?
+# for val in range (10,0,-1):
+#     print(val,end=" ")
+
+
+# while val >=1:
+#     print(val,end=" ")
+#     val-=1
+#print value from 1 to n ?
+
+# num=int(input("enter any number"))
+# for val in range(1,num+1,1):
+#     print(val,end=" ")
+
+# num = int(input("enter any value"))
+# val = 1
+# while val <= num:
+#     print(val,end=" ")
+#     val+=1
+
+#print value from n to 1 ?
+# num=int(input("enter any number"))
+# for val in range(num,0,-1):
+#     print(val,end=" ")
+
+# num = int(input("enter any value"))
+# val = 1
+# while num >=1:
+#     print(num,end=" ")
+#     num-=1
+
+
+#print even num  from 1 to 10 ?
+
+# for  val in range(1,11,1):
+#     if val%2==0:
+#         print(val,end=" ")
+# val = 1
+# while val<=10:
+#     if val%2==0:
+#         print(val,end=" ")
+#     val+=1
+
+#print odd num from 1 to 10 ?
+# for  val in range(1,11,1):
+#     if val%2!=0:
+#         print(val,end=" ")
+val = 1
+while val<=10:
+    if val%2!=0:
+        print(val,end=" ")
+    val+=1
+
+    
+
+
+
